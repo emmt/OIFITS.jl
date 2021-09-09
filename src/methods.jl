1644ff24978ce9f343128a24067cd08a918690a7
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2021-09-09 17:47:02 +0200

    Re-organize code and provide property `extname`

--- src/methods.jl
+++ src/methods.jl
@@ -64,6 +64,11 @@ for (T, field) in ((OIArray,      :arrname),
 end
 
 #------------------------------------------------------------------------------
+# ELEMENT TYPE CONVERSION
+
+eltype(x::Union{OIDataBlock,OIData}) = eltype(typeof(x))
+eltype(::Type{<:OIDataBlock{T}}) where {T} = T
+eltype(::Type{<:OIData{T}}) where {T} = T
 
 """
     change_eltype(S, T)
@@ -103,25 +108,64 @@ for D in (:OIDataBlock, :OITarget, :OIArray, :OIWavelength, :OICorr,
     end
 end
 
+convert(::Type{T}, A::T) where {T<:OIData} = A
+convert(::Type{OIData{T}}, A::OIData{T}) where {T<:AbstractFloat} = A
+function convert(::Type{OIData{T}}, A::OIData) where {T<:AbstractFloat}
+    # Create an empty data-set and first push independent data-blocks.
+    B = OIData{T}()
+    if isdefined(A, :target)
+        push!(B, A.target)
+    end
+    for db in A.array
+        push!(B, db)
+    end
+    for db in A.instr
+        push!(B, db)
+    end
+    for db in A.correl
+        push!(B, db)
+    end
+    # For other data-blocks, calling push! automatically set dependencies.
+    for db in A.vis
+        push!(B, db)
+    end
+    for db in A.vis2
+        push!(B, db)
+    end
+    for db in A.t3
+        push!(B, db)
+    end
+    for db in A.flux
+        push!(B, db)
+    end
+    for db in A.inspol
+        push!(B, db)
+    end
+    return B
+end
+
 #------------------------------------------------------------------------------
 # PROPERTIES
 
-setproperty!(x::OIDataBlock, f::Symbol, v) = _set_field!(x, f, v)
+propertynames(db::OIDataBlock) = (fieldnames(typeof(db))..., :extname)
 
 getproperty(db::OIDataBlock, sym::Symbol) = getproperty(db, Val(sym))
 getproperty(db::OIDataBlock, ::Val{S}) where {S} = getfield(db, S)
+getproperty(db::OIDataBlock, ::Val{:extname}) = extname(typeof(db))
 
 # Indirections to instrument (OI_WAVELENGTH) for OI_VIS, OI_VIS2, OI_T3, and OI_FLUX.
 for type in (:OIVis, :OIVis2, :OIT3, :OIFlux)
     @eval begin
+        @eval propertynames(db::$type) =
+            (fieldnames(typeof(db))..., :extname, :eff_wave, :eff_band)
         @eval getproperty(db::$type, ::Val{:eff_wave}) =
             getfield(db, :instr).eff_wave
         @eval getproperty(db::$type, ::Val{:eff_band}) =
             getfield(db, :instr).eff_band
-        @eval propertynames(db::$type) =
-            (fieldnames(typeof(db))..., :eff_wave, :eff_band)
     end
 end
+
+setproperty!(x::OIDataBlock, f::Symbol, v) = _set_field!(x, f, v)
 
 @inline _get_field(A, sym::Symbol, def=undef) =
     isdefined(A, sym) ? getfield(A, sym) : def
@@ -164,6 +208,154 @@ end
 end
 
 #------------------------------------------------------------------------------
+# BUILDING OF DATA-SETS
+
+function OIData(args::OIDataBlock...)
+    T = promote_type(map(eltype, args)...)
+    return OIData{T}(args...)
+end
+
+function OIData{T}(args::OIDataBlock...) where {T<:AbstractFloat}
+    # Create empty instance, then push the dependecy-less data-blocks and,
+    # finally, the data-blocks with dependencies.
+    data = OIData{T}()
+    for db in args
+        if !isa(db, DataBlocksWithDependencies)
+            push!(data, db)
+        end
+    end
+    for db in args
+        if isa(db, DataBlocksWithDependencies)
+            push!(data, db)
+        end
+    end
+    return data
+end
+
+# Copy the outer structure of an OI-FITS data-block.
+function copy(A::T) where {T<:OIDataBlock}
+    B = T()
+    for sym in fieldnames(T)
+        if isdefined(A, sym)
+            setfield!(B, sym, getfield(A, sym))
+        end
+    end
+    return B
+end
+
+function push!(data::OIData, args::OIDataBlock...)
+    for db in args
+        push!(data, db)
+    end
+    return data
+end
+
+# This version is to convert element type.
+push!(data::OIData{T}, db::OIDataBlock) where {T<:AbstractFloat} =
+    push!(data, OIDataBlock{T}(db))
+
+function push!(data::OIData{T}, db::OITarget{T}) where {T<:AbstractFloat}
+    isdefined(data, :target) && error("OI_TARGET already defined")
+    setfield!(data, :target, db)
+    return data
+end
+
+# Extend push! for OI_ARRAY, OI_WAVELENGTH, and OI_CORR.
+for (type, name, field) in ((:OIArray,      QuoteNode(:arrname),  QuoteNode(:array)),
+                            (:OIWavelength, QuoteNode(:insname),  QuoteNode(:instr)),
+                            (:OICorr,       QuoteNode(:corrname), QuoteNode(:correl)))
+    @eval begin
+        function push!(data::OIData{T}, db::$type{T}) where {T<:AbstractFloat}
+            isdefined(db, $name) || throw_undefined_field($name)
+            other = getfield(data, $field)[getfield(db, $name)]
+            if other === nothing
+                push!(getfield(data, $field), db)
+            elseif other != db # FIXME:
+                error(string("attempt to replace already existing ",
+                             extname($type), " named \"", name, "\""))
+            end
+            return data
+        end
+    end
+end
+
+for (type, field) in ((:OIVis,    :vis),
+                      (:OIVis2,   :vis2),
+                      (:OIT3,     :t3),
+                      (:OIFlux,   :flux),
+                      (:OIInsPol, :inspol))
+    @eval begin
+        function push!(data::OIData{T}, db::$type{T}) where {T<:AbstractFloat}
+            # Datablock must be copied on write to avoid side-effects.
+            copy_on_write = true
+
+            # Set/fix OI_WAVELENGTH dependency.
+            isdefined(db, :insname) || throw_undefined_field(:insname)
+            db, copy_on_write = _set_dependency!(
+                data.instr, db, copy_on_write, Val(:insname), Val(:instr))
+
+            # Set/fix OI_ARRAY dependency.
+            if isdefined(db, :arrname)
+                db, copy_on_write = _set_dependency!(
+                    data.array, db, copy_on_write, Val(:arrname), Val(:array))
+            end
+
+            # Set/fix OI_CORREL dependency.
+            if $type !== OIInsPol && isdefined(db, :corrname)
+                db, copy_on_write = _set_dependency!(
+                    data.correl, db, copy_on_write, Val(:corrname), Val(:correl))
+            end
+
+            push!(data.$field, db)
+            return data
+        end
+    end
+end
+
+@noinline throw_undefined_field(name::Union{AbstractString,Symbol}) =
+    throw(ErrorException("undefined field `$name`"))
+
+function _set_dependency!(deps::AbstractVector, # list of dependencies known by data-set
+                          db::T,                # datablock to be pushed on data-set
+                          copy_on_write::Bool,  # copy datablock on write?
+                          ::Val{name},          # field name of dependency name
+                          ::Val{value}          # field name of dependency value
+                          ) where {T<:OIDataBlock,name,value}
+
+    other = deps[getfield(db, name)]
+    if other === nothing
+        # Dependency not yet in list.
+        if isdefined(db, value)
+            push!(deps, getfield(db, value))
+        else
+            error("no ", extname(eltype(deps)), " named \"",
+                  fix_name(getfield(db, name)), "\" found")
+        end
+    else
+        # Another dependency with same name already in data-set.  Check that the
+        # two versions are identical.
+        setfield = true
+        if isdefined(db, value)
+            if getfield(db, value) === other
+                sefield = false
+            elseif getfield(db, value) == other
+                error("multiple ", extname(eltype(deps)), " named \"",
+                      fix_name(getfield(db, name)), "\" found")
+            end
+        end
+        if setfield
+            if copy_on_write
+                db = copy(db)
+                copy_on_write = false
+            end
+            setfield!(db, value, other)
+        end
+    end
+    return db, copy_on_write
+end
+
+#------------------------------------------------------------------------------
+# READING OF OI-FITS FILES
 
 const KeywordTypes = Union{Bool,Int,Cdouble,String}
 
@@ -251,8 +443,6 @@ end
     error("FITS table column \"", col, "\" of type ", S,
           " cannot be converted to type ", T)
 
-last_version(::Any) = 2
-
 """
     extname(db)
 
@@ -287,8 +477,6 @@ symbolic_extname(::Type{<:OIVis2}) = :OI_VIS2
 symbolic_extname(::Type{<:OIT3}) = :OI_T3
 symbolic_extname(::Type{<:OIFlux}) = :OI_FLUX
 symbolic_extname(::Type{<:OIInsPol}) = :OI_INSPOL
-
-Base.eltype(::Union{T,Type{T}}) where {S,T<:OIDataBlock{S}} = S
 
 get_format(::Union{T,Type{T}}, rev::Integer; kwds...) where {T<:OIDataBlock} =
     get_format(symbolic_extname(T), rev; kwds...)
@@ -338,153 +526,6 @@ function read!(dest::Union{OIData{T},Vector{OIDataBlock{T}}},
         end
     end
     return dest
-end
-
-# Copy the outer structure of an OI-FITS data-block.
-function copy(A::T) where {T<:OIDataBlock}
-    B = T()
-    for sym in fieldnames(T)
-        if isdefined(A, sym)
-            setfield!(B, sym, getfield(A, sym))
-        end
-    end
-    return B
-end
-
-function push!(data::OIData, args::OIDataBlock...)
-    for db in args
-        push!(data, db)
-    end
-    return data
-end
-
-# This version is to convert element type.
-push!(data::OIData{T}, db::OIDataBlock) where {T<:AbstractFloat} =
-    push!(data, OIDataBlock{T}(db))
-
-function push!(data::OIData{T}, db::OITarget{T}) where {T<:AbstractFloat}
-    isdefined(data, :target) && error("OI_TARGET already defined")
-    setfield!(data, :target, db)
-    return data
-end
-
-@noinline throw_undefined_field(name::Union{AbstractString,Symbol}) =
-    throw(ErrorException("undefined field `$name`"))
-
-# extend push! for OIArray, OIWavelength, and OICorr.
-for (type, name, field) in ((:OIArray,      QuoteNode(:arrname),  QuoteNode(:array)),
-                            (:OIWavelength, QuoteNode(:insname),  QuoteNode(:instr)),
-                            (:OICorr,       QuoteNode(:corrname), QuoteNode(:correl)))
-    @eval begin
-        function push!(data::OIData{T}, db::$type{T}) where {T<:AbstractFloat}
-            isdefined(db, $name) || throw_undefined_field($name)
-            other = getfield(data, $field)[getfield(db, $name)]
-            if other === nothing
-                push!(getfield(data, $field), db)
-            elseif other != db # FIXME:
-                error(string("attempt to replace already existing ",
-                             extname($type), " named \"", name, "\""))
-            end
-            return data
-        end
-    end
-end
-
-for (type, field) in ((:OIVis,    :vis),
-                      (:OIVis2,   :vis2),
-                      (:OIT3,     :t3),
-                      (:OIFlux,   :flux),
-                      (:OIInsPol, :inspol))
-    @eval begin
-        function push!(data::OIData{T}, db::$type{T}) where {T<:AbstractFloat}
-            # Datablock must be copied on write to avoid side-effects.
-            copy_on_write = true
-
-            # Set/fix OI_WAVELENGTH dependency.
-            isdefined(db, :insname) || throw_undefined_field(:insname)
-            db, copy_on_write = _set_dependency!(
-                data.instr, db, copy_on_write, Val(:insname), Val(:instr))
-
-            # Set/fix OI_ARRAY dependency.
-            if isdefined(db, :arrname)
-                db, copy_on_write = _set_dependency!(
-                    data.array, db, copy_on_write, Val(:arrname), Val(:array))
-            end
-
-            # Set/fix OI_CORREL dependency.
-            if $type !== OIInsPol && isdefined(db, :corrname)
-                db, copy_on_write = _set_dependency!(
-                    data.correl, db, copy_on_write, Val(:corrname), Val(:correl))
-            end
-
-            push!(data.$field, db)
-            return data
-        end
-    end
-end
-
-function _set_dependency!(deps::AbstractVector, # list of dependencies known by data-set
-                          db::T,                # datablock to be pushed on data-set
-                          copy_on_write::Bool,  # copy datablock on write?
-                          ::Val{name},          # field name of dependency name
-                          ::Val{value}          # field name of dependency value
-                          ) where {T<:OIDataBlock,name,value}
-
-    other = deps[getfield(db, name)]
-    if other === nothing
-        # Dependency not yet in list.
-        if isdefined(db, value)
-            push!(deps, getfield(db, value))
-        else
-            error("no ", extname(eltype(deps)), " named \"",
-                  fix_name(getfield(db, name)), "\" found")
-        end
-    else
-        # Another dependency with same name already in data-set.  Check that the
-        # two versions are identical.
-        setfield = true
-        if isdefined(db, value)
-            if getfield(db, value) === other
-                sefield = false
-            elseif getfield(db, value) == other
-                error("multiple ", extname(eltype(deps)), " named \"",
-                      fix_name(getfield(db, name)), "\" found")
-            end
-        end
-        if setfield
-            if copy_on_write
-                db = copy(db)
-                copy_on_write = false
-            end
-            setfield!(db, value, other)
-        end
-    end
-    return db, copy_on_write
-end
-
-function load!(dest::Vector{OIDataBlock{T}}, hdu::TableHDU) where {T<:AbstractFloat}
-    extn = read_keyword(String, hdu, "EXTNAME")
-    if extn !== nothing
-        if extn == "OI_TARGET"
-            push!(dest, _read(OITarget{T}, hdu))
-        elseif extn == "OI_ARRAY"
-            push!(dest, _read(OIArray{T}, hdu))
-        elseif extn == "OI_WAVELENGTH"
-            push!(dest, _read(OIWavelength{T}, hdu))
-        elseif extn == "OI_CORR"
-            push!(dest, _read(OICorr{T}, hdu))
-        elseif extn == "OI_VIS"
-            push!(dest, _read(OIVis{T}, hdu))
-        elseif extn == "OI_VIS2"
-            push!(dest, _read(OIVis2{T}, hdu))
-        elseif extn == "OI_T3"
-            push!(dest, _read(OIT3{T}, hdu))
-        elseif extn == "OI_FLUX"
-            push!(dest, _read(OIFlux{T}, hdu))
-        elseif extn == "OI_INSPOL"
-            push!(dest, _read(OIInsPol{T}, hdu))
-        end
-    end
 end
 
 _read(T::Type{<:OIDataBlock}, hdu::TableHDU) = _read!(T(), hdu)
